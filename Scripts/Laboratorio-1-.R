d3a8cabc38b5ef8a43d758e1344183a3b1843ae8
Cesarsaa <31328713+cesarsaa@users.noreply.github.com>
2017-09-20 20:32:29 -0500

    Script Laboratorio 1

--- Scripts/Laboratorio-1-.R
+++ Scripts/Laboratorio-1-.R
@@ -1,4 +1,5 @@
-# Laboratorio 1 Metodos Estadisticos 
+# Laboratorio 1 
+# Metodos Estadisticos 
 # Kevin Chica - Diana Arias - Cesar Saavedra 
 
 
@@ -13,32 +14,9 @@
 # ------------------------------------------------------- #
 
 
+
 # ------------------------------------------------------- #
 # Punto 5
 # ------------------------------------------------------- #
 
-# ------------------------------------------------------- #
-# 
-# ------------------------------------------------------- #
-N <- 5000
-n <- 5
-Nsim <- 100
-set.seed(84)
-a <- 3
-b <- 5
-Pob <- runif(N, a, b)
-
-#Se extraen muestra de n=5 y se repite el proceso 100 veces
-Muestra <- matrix(sample(Pob, n*Nsim), ncol = n) 
-head(Muestra)
-
-#Estimador 1: Promedio arítmetico
-T1 <- function(datos){
-  mean(datos)
-}
-
-#Estimador 2: (Máximo+Mínimo-1)
-T2 <- function(datos){
-  max(datos) + min(datos) - 1
-}
 
