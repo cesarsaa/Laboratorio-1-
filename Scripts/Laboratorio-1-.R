929275a660aad87ca70046fd75e1a0e22984045d
Cesarsaa <31328713+cesarsaa@users.noreply.github.com>
2017-09-20 20:30:42 -0500

    R2

--- Scripts/Laboratorio-1-.R
+++ Scripts/Laboratorio-1-.R
@@ -27,3 +27,18 @@ set.seed(84)
 a <- 3
 b <- 5
 Pob <- runif(N, a, b)
+
+#Se extraen muestra de n=5 y se repite el proceso 100 veces
+Muestra <- matrix(sample(Pob, n*Nsim), ncol = n) 
+head(Muestra)
+
+#Estimador 1: Promedio arítmetico
+T1 <- function(datos){
+  mean(datos)
+}
+
+#Estimador 2: (Máximo+Mínimo-1)
+T2 <- function(datos){
+  max(datos) + min(datos) - 1
+}
+
